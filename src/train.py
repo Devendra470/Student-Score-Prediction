ae345526f4a05095d40935483b484fe38a32e105
Devendra470 <devendra12052005@gmail.com>
2026-06-28 00:45:02 +0530

    Updated Files and changes folder structure

--- src/train.py
+++ src/train.py
@@ -1,20 +1,42 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 import joblib
 
-df=pd.read_csv("../data/raw/Student_Performance.csv")
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-df["Extracurricular Activities"]=df["Extracurricular Activities"].map({"Yes":1,"No":0})
 
-X=df.drop("Performance Index",axis=1)
-y=df["Performance Index"]
+def train_model():
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+    # Load Dataset
+    df = pd.read_csv("../data/raw/Student_Performance.csv")
 
-model=LinearRegression()
-model.fit(X_train,y_train)
+    # Encode Categorical Feature
+    df["Extracurricular Activities"] = df["Extracurricular Activities"].map({
+        "Yes": 1,
+        "No": 0
+    })
 
-joblib.dump(model,"../models/linear_regression.pkl")
-print("Model Saved Sucessfully")
+    # Features and Target
+    X = df.drop("Performance Index", axis=1)
+    y = df["Performance Index"]
+
+    # Split Dataset
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
+
+    # Train Model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Save Model
+    joblib.dump(model, "../models/linear_regression.pkl")
+
+    print("✅ Model trained and saved successfully!")
+
+
+if __name__ == "__main__":
+    train_model()
